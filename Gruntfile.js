6a27dcb0c534e634560c132646d40056334ccc95
Gavin Lloyd <gavinhungry@gmail.com>
2013-09-15 21:31:27 -0700

    clean up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,10 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     less: {
-
       production: {
-        options: {
-          yuicompress: true
-        },
-        files: {
-          'client/css/jsbyte.min.css': 'client/css/jsbyte.less'
-        }
+        options: { yuicompress: true },
+        files: { 'client/css/jsbyte.min.css': 'client/css/jsbyte.less' }
       }
-
     }
   });
 
