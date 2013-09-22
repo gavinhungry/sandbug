e8f848bf07844fada537b06d2ed522f53b0da40b
Gavin Lloyd <gavinhungry@gmail.com>
2013-09-21 21:22:06 -0700

    use Backbone for filtering CDN results

--- client/js/cdn.js
+++ client/js/cdn.js
@@ -4,16 +4,14 @@
  * cdn.js: client-side CDN cache and filter
  */
 
-define(['jquery', 'underscore', 'backbone', 'config', 'utils'],
-function($, _, Backbone, config, utils) {
+define([
+  'jquery', 'underscore', 'backbone', 'templates', 'dom', 'config', 'utils'
+],
+function($, _, Backbone, templates, dom, config, utils) {
   'use strict';
 
   var cdn = utils.module('cdn');
-
   var cache = null;
-  var results_visible = false;
-  var initialized = false;
-  var $results;
 
   /**
    * Check if client-side CDN package cache exists
@@ -28,7 +26,7 @@ function($, _, Backbone, config, utils) {
    * Return a cached version of CDN packages
    *
    * @param {Boolean} update: if true, force the cache to update first
-   * @return {Promise}: promise to return the CDN packages
+   * @return {Promise}: promise to return the CDN packages {Array}
    */
   cdn.get_cache = function(update) {
     var d = $.Deferred();
@@ -37,9 +35,9 @@ function($, _, Backbone, config, utils) {
       d.resolve(cache);
     } else {
       cdn.update_cache().done(function(packages) {
-        d.resolve(packages);
+        d.resolve(utils.ensure_array(packages));
       }).fail(function(err) {
-        d.resolve(cdn.cache_exists() ? cache : []);
+        d.resolve(utils.ensure_array(cache));
       });
     }
 
@@ -65,87 +63,120 @@ function($, _, Backbone, config, utils) {
   };
 
   // FilterResult: Model
-  // FilterResults: Collection of FilterResult
-  // FilterResultView: li
-  // FilterResultsView: <div><ol>
+  cdn.FilterResult = Backbone.Model.extend({
+    // defaults
+  });
 
+  // FilterResultView: li
+  cdn.FilterResultView = Backbone.View.extend({
+    template: 'cdn-result',
+    tagName: 'li',
+
+    initialize: function() {
+      this.render();
+    },
+
+    events: {
+      'click': 'add_lib'
+    },
+
+    add_lib: function(e) {
+      // add lib to markup panel
+      utils.log('add lib to markup panel');
+    },
+
+    render: function() {
+      templates.get(this.template, function(template) {
+        var html = _.template(template, { pkg: this.model.toJSON() });
+        this.$el.html(html);
+      }, this);
+      return this;
+    }
+  });
+
+  // FilterResults: Collection of FilterResult
+  cdn.FilterResults = Backbone.Collection.extend({
+    model: cdn.FilterResult,
+
+    initialize: function(models, options) {
+
+    }
+  });
+
+  // FilterResultsView: <div><ol>
+  cdn.FilterResultsView = Backbone.View.extend({
+    template: 'cdn-results',
+    el: '#cdn-results',
+
+    initialize: function() {
+      this.render();
+    },
+
+    show: function() {
+      this.$el.css({ 'display': 'block' });
+
+      // adjust margins when overflowing
+      var overflow = this.$ol[0].scrollHeight > this.$ol[0].offsetHeight;
+      this.$ol.toggleClass('overflow', overflow);
+
+      this.$el.css({ 'opacity': 1 });
+    },
+
+    hide: function() {
+      this.$el.transition({ 'opacity': 0 }, 'fast', function() {
+        this.css({ 'display': 'none' });
+      });
+    },
+
+    render: function() {
+      templates.get(this.template, function(template) {
+        var html = template;
+        this.$el.html(html);
+
+        this.$ol = this.$el.children('ol');
+        dom.cache(this, this.$el, {
+          'by_class': ['results']
+        });
+
+        _.each(this.collection.models, function(result) {
+          var rv = new cdn.FilterResultView({ model: result });
+          this.$results.append(rv.el);
+        }, this);
+
+        this.show();
+      }, this);
+
+      return this;
+    }
+  });
 
   /**
-   * Init the CDN input for filterable library selection
    *
-   * @param {jQuery} $cdn: CDN input element
+   * @param {jQuery} $cdn:
    */
   cdn.init_filter = function($cdn) {
-    if (initialized) {
-      utils.log('CDN filter already initialized');
-      return;
-    }
+    cdn.get_cache().done(function(packages) {
+      var resultsCol = new cdn.FilterResults(packages);
+      var resultsView = new cdn.FilterResultsView({ collection: resultsCol });
+    });
+  };
 
-    utils.log('initializing CDN filter');
-
-    $results = $cdn.next('.results');
-    var $ol = $results.children('ol');
-    var $nomatch = $results.children('.nomatch');
-
+  /*
     $cdn.on('keyup', _.debounce(function(e) {
       var filter = _.trim($cdn.val().toLowerCase());
 
-      // hide results when there is no filter
-      if (!filter) { return cdn.hide_results(); }
-
-      results_visible = true;
-
-      // build filtered, sorted list of packages
       cdn.get_cache().done(function(packages) {
+
+        // filter for the packages that match the input
         var filtered = _.filter(packages, function(pkg) {
           return _.str.include(pkg.name.toLowerCase(), filter);
         });
 
+        // sort filtered packages by comparing them with the filter string
         var sorted = _.sortBy(filtered, function(pkg) {
           return _.levenshtein(filter, pkg.name.toLowerCase());
         });
-
-        $ol.empty();
-        _.each(sorted, function(pkg) {
-          var liStr = _.sprintf('<li>%s <span class="right">%s</span></li>',
-            pkg.name, pkg.version);
-
-          var $pkg = $(liStr).attr('data-filename', pkg.filename);
-          $ol.append($pkg);
-        });
-
-        $nomatch.toggleClass('hide', !!sorted.length);
-        $results.css({ 'display': 'block' });
-
-        // adjust margins when overflowing
-        $ol.toggleClass('overflow', $ol[0].scrollHeight > $ol[0].offsetHeight);
-
-        $results.css({ 'opacity': 1 });
-      });
-    }, 10));
-
-    // only enable the filter input if there are packages in the cache
-    cdn.get_cache().done(function(packages) {
-      $cdn.prop('disabled', !packages.length);
-    });
-
-    initialized = true;
-  };
-
-  /**
-   * Hide the CDN results box
-   */
-  cdn.hide_results = function() {
-    if (!initialized) {
-      utils.log('CDN filter not yet initialized');
-      return;
-    }
-
-    results_visible = false;
-    $results.transition({ 'opacity': 0 }, 'fast', function() {
-      if (!results_visible) { $results.css({ 'display': 'none' }); }
-    });
-  };
+  */
 
   return cdn;
 });
