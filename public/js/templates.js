e79be83d9bfdd5e9d5c2adbe4c4658c2b6172660
Gavin Lloyd <gavinhungry@gmail.com>
2013-10-16 09:17:46 -0700

    cleanup template manager

--- public/js/templates.js
+++ public/js/templates.js
@@ -11,31 +11,27 @@ function(config, utils, $, _) {
   var templates = utils.module('templates');
 
   var dir = '/templates';
-  var load_cache = {}; // promises
-  var template_cache = {}; // promises
+  var cache = {}; // promise cache
 
   /**
-   * Load template(s) into cache (preload)
+   * Load compiled template function(s) into cache
    *
    * @param {String | Array} - template(s) to load
    */
   templates.load = function(ids) {
     _.each(utils.ensure_array(ids), function(id) {
-      if (template_cache[id]) { return; }
+      if (cache[id]) { return; }
 
-      var templateUri = dir + '/' + id + '.html';
+      var template_deferred = $.Deferred();
+      cache[id] = template_deferred.promise();
+
+      var templateUri = _.sprintf('%s/%s.html', dir, id);
       if (config.debug) { templateUri += '?v=' + (new Date()).getTime(); }
 
-      var load_promise = $.get(templateUri);
-      var template_deferred = $.Deferred();
-
-      load_promise.done(function(template) {
-        var template_fn = _.template(template);
-        template_deferred.resolve(template_fn)
+      $.get(templateUri).done(function(template) {
+        // resolve with compiled template function
+        template_deferred.resolve(_.template(template));
       });
-
-      load_cache[id] = load_promise;
-      template_cache[id] = template_deferred.promise();
     });
   };
 
@@ -45,13 +41,13 @@ function(config, utils, $, _) {
    *
    * @param {String} id - template id
    * @param {Object} [context] - context to bind to done callbacks
-   * @return {Promise} promise to return the compiled _.template function
+   * @return {Promise} promise to return the compiled template function
    */
   templates.get = function(id, context) {
     templates.load(id);
     var d = $.Deferred();
 
-    template_cache[id].done(function(template_fn) {
+    cache[id].done(function(template_fn) {
       d.resolveWith(context || null, [template_fn]);
     });
 
