a395b0bbad9535820ec60de958f4e5e34da59fbe
Gavin Lloyd <gavinhungry@gmail.com>
2013-10-20 01:30:56 -0700

    config.option for creating new options

--- public/js/config.js
+++ public/js/config.js
@@ -23,9 +23,18 @@ function($, _) {
     'frame': 'http://localhost:8081/' // https://frame.debugger.io/
   };
 
-  // options may always be functions, but any option originally declared as a
-  // boolean must either be a boolean or a function that returns a boolean
-  _.each(options, function(value, option) {
+  /**
+   * Create a new config option
+   *
+   * Values may always be functions, but any value originally declared as a
+   * boolean must either be a boolean or a function that returns a boolean
+   *
+   * @param {String} option - option name
+   * @param {Mixed} value - initial value
+   */
+  config.option = function(option, value) {
+    if (config.hasOwnProperty(option)) { return; }
+
     var isBool = _.isBoolean(value);
 
     Object.defineProperty(config, option, {
@@ -39,7 +48,11 @@ function($, _) {
         options[option] = (isBool && !isFn) ? !!val : val;
       }
     });
-  });
+
+    config[option] = value;
+  };
+
+  _.each(options, function(value, option) { config.option(option, value); });
 
   if (config.debug) { window.config = config; }
   return config;
