9a2d91a74c39ab265327472c973b3949223dd67d
Gavin Lloyd <gavinhungry@gmail.com>
2013-10-22 17:05:47 -0700

    use shared require.js config via window._debugger_io_require

--- public/main.js
+++ public/main.js
@@ -4,58 +4,7 @@
 
 (function() {
   'use strict';
-  var CDNJS = '//cdnjs.cloudflare.com/ajax/libs';
-
-  requirejs.config({
-    baseUrl: '/js',
-    urlArgs: 'v=' + (new Date()).getTime(),
-
-    paths: {
-      // libraries
-      jqueryjs: CDNJS + '/jquery/2.0.3/jquery.min',
-      underscorejs: CDNJS + '/underscore.js/1.5.2/underscore-min',
-      backbonejs: CDNJS + '/backbone.js/1.0.0/backbone-min',
-      codemirrorjs: CDNJS + '/codemirror/3.16.0/codemirror.min',
-
-      // plugins
-      ui: CDNJS + '/jqueryui/1.10.3/jquery-ui.min',
-      transit: CDNJS + '/jquery.transit/0.9.9/jquery.transit.min',
-      nano: CDNJS + '/jquery.nanoscroller/0.7.4/jquery.nanoscroller.min',
-      string: CDNJS + '/underscore.string/2.3.3/underscore.string.min',
-      inflection: 'plugins/underscore.inflection.min',
-      codemirror_js: CDNJS + '/codemirror/3.16.0/mode/javascript/javascript.min',
-      codemirror_css: CDNJS + '/codemirror/3.16.0/mode/css/css.min',
-      codemirror_xml: CDNJS + '/codemirror/3.16.0/mode/xml/xml.min',
-      codemirror_html: CDNJS + '/codemirror/3.16.0/mode/htmlmixed/htmlmixed.min',
-      codemirror_search: CDNJS + '/codemirror/3.16.0/addon/search/searchcursor.min',
-
-      // libraries with plugins
-      jquery: 'lib/jquery',
-      underscore: 'lib/underscore',
-      backbone: 'lib/backbone',
-      codemirror: 'lib/codemirror'
-    },
-
-    shim: {
-      // libraries
-      jqueryjs: { exports: '$' },
-      underscorejs: { exports: '_' },
-      backbonejs: { deps: ['jqueryjs', 'underscorejs'], exports: 'Backbone' },
-      codemirrorjs: { exports: 'CodeMirror' },
-
-      // plugins
-      ui: { deps: ['jqueryjs'], exports: '$.ui' },
-      transit: { deps: ['jqueryjs'], exports: '$.transit' },
-      nano: { deps: ['jqueryjs'], exports: '$.fn.nanoScroller' },
-      string: { deps: ['underscorejs'], exports: '_.str' },
-      inflection: { deps: ['underscorejs'] },
-      codemirror_js: { deps: ['codemirrorjs'] },
-      codemirror_css: { deps: ['codemirrorjs'] },
-      codemirror_xml: { deps: ['codemirrorjs'] },
-      codemirror_html: { deps: ['codemirrorjs'] },
-      codemirror_search: { deps: ['codemirrorjs'] }
-    }
-  });
+  window._debugger_io_require(null, true);
 
   require(['jquery', 'app'],
   function($, app) {
--- public/test/test.js
+++ public/test/test.js
@@ -4,70 +4,10 @@
 
 (function() {
   'use strict';
-  var CDNJS = '//cdnjs.cloudflare.com/ajax/libs';
+  window._debugger_io_require('../', true);
 
-  requirejs.config({
-    baseUrl: '/../js',
-    urlArgs: 'v=' + (new Date()).getTime(),
-
-    paths: {
-      // libraries
-      jqueryjs: CDNJS + '/jquery/2.0.3/jquery.min',
-      underscorejs: CDNJS + '/underscore.js/1.5.2/underscore-min',
-      backbonejs: CDNJS + '/backbone.js/1.0.0/backbone-min',
-      codemirrorjs: CDNJS + '/codemirror/3.16.0/codemirror.min',
-
-      // plugins
-      ui: CDNJS + '/jqueryui/1.10.3/jquery-ui.min',
-      transit: CDNJS + '/jquery.transit/0.9.9/jquery.transit.min',
-      nano: CDNJS + '/jquery.nanoscroller/0.7.4/jquery.nanoscroller.min',
-      string: CDNJS + '/underscore.string/2.3.3/underscore.string.min',
-      inflection: 'plugins/underscore.inflection.min',
-      codemirror_js: CDNJS + '/codemirror/3.16.0/mode/javascript/javascript.min',
-      codemirror_css: CDNJS + '/codemirror/3.16.0/mode/css/css.min',
-      codemirror_xml: CDNJS + '/codemirror/3.16.0/mode/xml/xml.min',
-      codemirror_html: CDNJS + '/codemirror/3.16.0/mode/htmlmixed/htmlmixed.min',
-      codemirror_search: CDNJS + '/codemirror/3.16.0/addon/search/searchcursor.min',
-
-      // testing
-      jasmine: CDNJS + '/jasmine/1.3.1/jasmine',
-      jasmine_html: CDNJS + '/jasmine/1.3.1/jasmine-html',
-
-      // libraries with plugins
-      jquery: 'lib/jquery',
-      underscore: 'lib/underscore',
-      backbone: 'lib/backbone',
-      codemirror: 'lib/codemirror'
-    },
-
-    shim: {
-      // libraries
-      jqueryjs: { exports: '$' },
-      underscorejs: { exports: '_' },
-      backbonejs: { deps: ['jqueryjs', 'underscorejs'], exports: 'Backbone' },
-      codemirrorjs: { exports: 'CodeMirror' },
-
-      // plugins
-      ui: { deps: ['jqueryjs'], exports: '$.ui' },
-      transit: { deps: ['jqueryjs'], exports: '$.transit' },
-      nano: { deps: ['jqueryjs'], exports: '$.fn.nanoScroller' },
-      string: { deps: ['underscorejs'], exports: '_.str' },
-      inflection: { deps: ['underscorejs'] },
-      codemirror_js: { deps: ['codemirrorjs'] },
-      codemirror_css: { deps: ['codemirrorjs'] },
-      codemirror_xml: { deps: ['codemirrorjs'] },
-      codemirror_html: { deps: ['codemirrorjs'] },
-      codemirror_search: { deps: ['codemirrorjs'] },
-
-      // testing
-      jasmine: { exports: 'jasmine' },
-      jasmine_html: { deps: ['jasmine'], exports: 'jasmine' }
-    }
-  });
-
-  require(['jquery', 'jasmine_html', '../test/matchers', 'underscore'],
-  function($, jasmine, matchers, _) {
-
+  require(['jquery', 'underscore', 'jasmine', '../test/matchers'],
+  function($, _, jasmine, matchers) {
     var env = jasmine.getEnv();
     env.addReporter(new jasmine.HtmlReporter());
 
