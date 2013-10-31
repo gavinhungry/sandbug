de3abd621c999a8e3131a62a8de65f29d3b24d72
Gavin Lloyd <gavinhungry@gmail.com>
2013-10-31 00:29:29 -0700

    popups.remove_all

--- public/js/popups.js
+++ public/js/popups.js
@@ -20,7 +20,7 @@ function(config, utils, $, _, bus, templates) {
    * @param {Object} [data] - contextual data to the templating function
    * @return {Promise} promise to return the HTML for the popup
    */
-  popups.build_popup = function(name, data) {
+  popups.build = function(name, data) {
     var d = $.Deferred();
 
     var content = _.sprintf('popup-%s', name);
@@ -31,8 +31,40 @@ function(config, utils, $, _, bus, templates) {
       var popupHtml = popup_fn({ content: contentHtml });
 
       d.resolve(popupHtml);
-    }).fail(function(a, b, c) {
+    }).fail(function(err) {
       // uh oh
+    });
+
+    return d.promise();
+  };
+
+  /**
+   * Fade hide and remove all popups on the page
+   *
+   * @return {Promise} promise to return the number of removed popups
+   */
+  popups.remove_all = function() {
+    var d = $.Deferred();
+
+    var $popups = $('body').children('.popup');
+    var len = $popups.length;
+
+    var promises = [];
+
+    _.each($popups, function(popupNode) {
+      var $popup = $(popupNode);
+      var popup_deferred = $.Deferred();
+
+      $popup.transition({ 'opacity': 0 }, 'fast', function() {
+        $popup.remove();
+        popup_deferred.resolve(true);
+      });
+
+      promises.push(popup_deferred.promise());
+    });
+
+    $.when.apply(null, promises).done(function() {
+      d.resolve(len);
     });
 
     return d.promise();
