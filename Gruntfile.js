a6ee349f5f648117b8f8f099d3ad0097b48d50d5
Gavin Lloyd <gavinhungry@gmail.com>
2013-12-12 13:54:59 -0800

    simplify comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,9 @@ module.exports = function(grunt) {
 
             codemirror_js: 'empty:',
             codemirror_coffeescript: 'empty:'
-          }
+          },
+
+          exclude: ['promise']
         }
       }
     },
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,10 +1,5 @@
-/**
+/*
  * debugger.io: An interactive web scripting sandbox
- *
- * https://github.com/gavinhungry/debugger.io
- *
- * Copyright (C) 2013 Gavin Lloyd <gavinhungry@gmail.com>
- * This is free software distributed under the terms of the MIT license
  */
 
 (function() {
