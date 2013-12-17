bc1e3d4eb0a74887304569e8fdba4c5fc5e49365
Gavin Lloyd <gavinhungry@gmail.com>
2013-12-16 17:58:53 -0800

    Move routes to server/js/routes.js

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@
 
 define([
   'module', 'path', 'config', 'utils', 'underscore', 'q',
-  'auth', 'cdn', 'consolidate', 'express', './frame'
+  'auth', 'consolidate', 'express', './frame', 'routes'
 ],
 function(
   module, path, config, utils, _, Q,
-  auth, cdn, cons, express, frame
+  auth, cons, express, frame, routes
 ) {
   'use strict';
 
@@ -31,32 +31,11 @@ function(
   server.set('view engine', 'html');
   server.set('views', __dirname + '/templates');
 
-  server.get('/', function(req, res) {
-    var user = req.user || {};
-    res.render('index', {
-      prod: config.prod,
-      rev: config.build.rev
-    });
-  });
-
-  // GET /cdn - list of CDN packages
-  server.get('/cdn', function(req, res) {
-    cdn.get_cache().done(function(packages) {
-      res.json(packages);
-    });
-  });
-
-  // GET /config - additional client-side config options
-  server.get('/config', function(req, res) {
-    res.json(config.client);
-  });
-
-  // POST /login
-  server.post('/login', auth.authenticate, function(req, res) {
-    var user = req.user || {};
-    var username = auth.sanitize_username(user.username);
-    res.json(username);
-  });
+  // routes
+  server.get('/', routes.get.index);
+  server.get('/cdn', routes.get.cdn);
+  server.get('/config', routes.get.config);
+  server.post('/login', auth.authenticate, routes.post.login);
 
   return app;
 });
---
+++ server/js/routes.js
@@ -0,0 +1,47 @@
+/*
+ * debugger.io: An interactive web scripting sandbox
+ */
+
+define([
+  'module', 'path', 'config', 'utils', 'underscore', 'q',
+  'auth', 'cdn'
+],
+function(
+  module, path, config, utils, _, Q,
+  auth, cdn
+) {
+  'use strict';
+
+  var __dirname = path.dirname(module.uri);
+  var routes = { get: {}, post: {} };
+
+  // GET /
+  routes.get.index = function(req, res) {
+    var user = req.user || {};
+    res.render('index', {
+      prod: config.prod,
+      rev: config.build.rev
+    });
+  };
+
+  // GET /cdn - list of CDN packages
+  routes.get.cdn = function(req, res) {
+    cdn.get_cache().done(function(packages) {
+      res.json(packages);
+    });
+  };
+
+  // GET /config - additional client-side config options
+  routes.get.config = function(req, res) {
+    res.json(config.client);
+  };
+
+  // POST /login
+  routes.post.login = function(req, res) {
+    var user = req.user || {};
+    var username = auth.sanitize_username(user.username);
+    res.json(username);
+  };
+
+  return routes;
+});
