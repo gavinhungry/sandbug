193aecce15f74e9a98492fbca36cfe6ec31865ec
Gavin Lloyd <gavinhungry@gmail.com>
2014-01-25 15:59:32 -0800

    Add user.js

---
+++ public/src/js/user.js
@@ -0,0 +1,31 @@
+/*
+ * debugger.io: An interactive web scripting sandbox
+ *
+ * user.js: user actions
+ */
+
+define([
+  'config', 'utils', 'jquery', 'underscore',
+  'bus', 'flash'
+],
+function(
+  config, utils, $, _, Backbone,
+  bus, flash
+) {
+  'use strict';
+
+  var user = utils.module('user');
+
+  /**
+   * Log the current user out
+   */
+  user.logout = function() {
+    $.post('/api/logout', { _csrf: config.csrf }).done(function(data) {
+      bus.trigger('user:logout');
+    }).fail(function() {
+      flash.message_bad(locales.string('logout_error'));
+    });
+  };
+
+  return user;
+});
