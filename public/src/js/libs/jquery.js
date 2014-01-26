249f356c5752641b9850290182b1e9c7e92fabcf
Gavin Lloyd <gavinhungry@gmail.com>
2014-01-25 22:53:00 -0800

    Add $.fn.transitIn, $.fn.transitOut

--- public/src/js/libs/jquery.js
+++ public/src/js/libs/jquery.js
@@ -5,12 +5,26 @@
 /**
  * jQuery
  *
- * Includes: Transit, nanoScroller.js
+ * Includes: Transit, nanoScroller.js, transitIn/transitOut
  */
 define(['jqueryjs', 'transit', 'nano'],
 function($, transit, nano) {
   // jQuery.transit fallback to $.fn.animate
   if (!$.support.transition) { $.fn.transition = $.fn.animate; }
+
+  $.fn.transitIn = function(speed, easing, callback) {
+    this.css({ display: 'block' });
+    this.transition({ opacity: 1 }, speed, easing, function() {
+      typeof callback === 'function' && callback.call(this);
+    });
+  };
+
+  $.fn.transitOut = function(speed, easing, callback) {
+    this.transition({ opacity: 0 }, speed, easing, function() {
+    this.css({ display: 'none' });
+      typeof callback === 'function' && callback.call(this);
+    });
+  };
 
   return $; // $.noConflict(false);
 });
