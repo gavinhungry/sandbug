46eda067b1f1f2a353dfd36bfb4e2228364438ab
Gavin Lloyd <gavinhungry@gmail.com>
2014-02-07 14:43:29 -0800

    Add Bug schema/model with Mongoose

--- server/js/bugs.js
+++ server/js/bugs.js
@@ -4,16 +4,95 @@
 
 define([
   'module', 'path', 'config', 'utils', 'underscore', 'q',
-  'auth', 'db'
+  'auth', 'db', 'mongoose'
 ],
 function(
   module, path, config, utils, _, Q,
-  auth, db
+  auth, db, mongoose
 ) {
   'use strict';
 
   var __dirname = path.dirname(module.uri);
   var bugs = {};
+
+  var mongoose_p = Q.defer();
+  mongoose.connect(db.dsn).connection
+  .once('error', mongoose_p.reject)
+  .once('open', function() {
+
+    var sanitize_username = function(str) {
+      return auth.sanitize_username(str) || null;
+    };
+
+    var random_slug = function() {
+      return '4'; // FIXME
+    };
+
+    var bugSchema = mongoose.Schema({
+      username: { type: String, filter: sanitize_username },
+      slug: { type: String, filter: _.slugify, default: random_slug },
+      title: { type: String, default: 'Bug' },
+
+      created: { type: Date, default: Date.now },
+      updated: { type: Date, default: Date.now },
+
+      secret: { type: Boolean, default: false },
+      collaborators: { type: [String], default: [] },
+
+      map: {
+        markup: {
+          mode: { type: String, enum: ['htmlmixed', 'gfm', 'jade', 'haml'] },
+          content: { type: String, default: '' }
+        },
+
+        style: {
+          mode: { type: String, enum: ['css', 'less', 'scss'] },
+          content: { type: String, default: '' }
+        },
+
+        script: {
+          mode: {
+            type: String,
+            enum: [
+              'javascript', 'traceur', 'coffeescript', 'typescript',
+              'gorillascript'
+            ]
+          },
+          content: { type: String, default: '' }
+        }
+      }
+    });
+
+    bugSchema.pre('validate', function(next) {
+      var model = this;
+
+      _.chain(bugSchema.paths).filter(function(key) {
+        return _.first(key.path) !== '_';
+      }).each(function(key) {
+
+        var split = key.path.split('.');
+        var last = split.pop();
+        var base = utils.reduce(split.join('.'), model);
+
+        // use first enum value as default
+        if (key.enumValues && key.enumValues.length &&
+          !_.contains(key.enumValues, model[key.path])) {
+            base[last] = _.first(key.enumValues);
+        }
+
+        // allow for a filter function on strings
+        if (key.options.type === String && _.isFunction(key.options.filter)) {
+           base[last] = key.options.filter(model[key.path]);
+        }
+      });
+
+      next();
+    });
+
+    bugs.Bug = mongoose.model('bug', bugSchema);
+
+    mongoose_p.resolve();
+  });
 
   /**
    * Get a bug from a username and slug
