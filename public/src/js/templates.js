183f220a39895b53ca7c14c7ee78d5bbd3727e22
Gavin Lloyd <gavinhungry@gmail.com>
2014-08-01 17:42:36 -0700

    Use Handlebars

--- public/src/js/templates.js
+++ public/src/js/templates.js
@@ -13,6 +13,8 @@ define(function(require) {
   var config = require('config');
   var utils  = require('utils');
 
+  var Handlebars = require('handlebars');
+
   // ---
 
   var templates = utils.module('templates');
@@ -43,7 +45,7 @@ define(function(require) {
 
       $.get(templateUri).done(function(template, status, xhr) {
         // resolve with compiled template function
-        var template_fn = _.template(template);
+        var template_fn = Handlebars.compile(template);
 
         d.resolve(function(data) {
           // extend data passed to template with locals
